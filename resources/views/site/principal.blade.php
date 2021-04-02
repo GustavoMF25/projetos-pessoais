5706a79e7cdef6a3051c07410c54c6608636c8b6
gustavo.fernandes <gustavomarcelinofernadnes25@gmail.com>
2021-04-02 18:49:32 -0300

    Mudando cores

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -49,26 +49,27 @@
         </section>
 
         <section class="bg-secondary">
-            <div class="row">
-                <div class="col-5 text-center d-flex align-items-center justify-content-center">
-
-                    <div class="">
-                        <h2><strong>Contato</strong></h2>
-                        <img class="img-fluid" src=" {{ asset('img/logo-icon.png') }}" alt="">
-                        <h1 class="text-uppercase"><strong>Sistema de Super Gestão</strong></h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 text-center d-flex align-items-center justify-content-center">
+                        <div>
+                            <h2><strong>Contato</strong></h2>
+                            <img class="img-fluid" src=" {{ asset('img/logo-icon.png') }}" alt="">
+                            <h1 class="text-uppercase"><strong>Sistema de Super Gestão</strong></h1>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-6">
+                    <div class="col-lg-6">
 
-                    <h2>Contato</h2>
+                        <h2><strong>Contato</strong></h2>
 
-                    <p>
-                        Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo
-                        formulário abaixo.
-                    </p>
-                    @component('site.layouts._components.form_contato')
-                    @endcomponent
+                        <p>
+                            Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo
+                            formulário abaixo.
+                        </p>
+                        @component('site.layouts._components.form_contato')
+                        @endcomponent
 
+                    </div>
                 </div>
             </div>
         </section>
